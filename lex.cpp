8d0af0764ded2687f7ac5aaaa263df41b81704ce
Bhavya Patel <bhavyapatel21999@gmail.com>
2024-09-08 15:09:52 +0530

    basic lexer

--- lex.cpp
+++ lex.cpp
@@ -1,42 +1,9 @@
 #include <iostream> // Use <iostream> for C++
+#include <optional>
+#include <ostream>
+#include <string>
 
-class LEX
-{
-
-private:
-  std::string source;
-  char curChar;
-  int curPos;
-
-public:
-  LEX(const std::string &src) : source(src + '\n'), curChar('\0'), curPos(-1)
-  {
-    nextChar();
-  };
-  void nextChar()
-  {
-    curPos++;
-    if (curPos >= static_cast<int>(source.length()))
-    {
-      curChar = '\0';
-    }
-    else
-    {
-      curChar = source[curPos];
-    }
-  }
-  char peek()
-  {
-    if (curPos + 1 >= static_cast<int>(source.length()))
-    {
-      return '\0';
-    }
-    return source[curPos + 1];
-  }
-  char getCurrentChar() const { return curChar; }
-};
-enum class TokenType
-{
+enum class TokenType {
 
   EOF_ = -1,
   NEWLINE = 0,
@@ -69,28 +36,179 @@ enum class TokenType
   GTEQ = 211
 
 };
-void printTokenType(TokenType token)
-{
-  if (token == TokenType::EQ)
-  {
-    std::cout << "Token is EQ (equals)" << std::endl;
+std::string tokenTypeToString(TokenType type) {
+  switch (type) {
+  case TokenType::EOF_:
+    return "EOF";
+  case TokenType::NEWLINE:
+    return "NEWLINE";
+  case TokenType::NUMBER:
+    return "NUMBER";
+  case TokenType::IDENT:
+    return "IDENT";
+  case TokenType::STRING:
+    return "STRING";
+  case TokenType::LABEL:
+    return "LABEL";
+  case TokenType::GOTO:
+    return "GOTO";
+  case TokenType::PRINT:
+    return "PRINT";
+  case TokenType::INPUT:
+    return "INPUT";
+  case TokenType::LET:
+    return "LET";
+  case TokenType::IF:
+    return "IF";
+  case TokenType::THEN:
+    return "THEN";
+  case TokenType::ENDIF:
+    return "ENDIF";
+  case TokenType::WHILE:
+    return "WHILE";
+  case TokenType::REPEAT:
+    return "REPEAT";
+  case TokenType::ENDWHILE:
+    return "ENDWHILE";
+  case TokenType::EQ:
+    return "EQ";
+  case TokenType::PLUS:
+    return "PLUS";
+  case TokenType::MINUS:
+    return "MINUS";
+  case TokenType::ASTERISK:
+    return "ASTERISK";
+  case TokenType::SLASH:
+    return "/SLASH";
+  case TokenType::EQEQ:
+    return "EQEQ";
+  case TokenType::NOTEQ:
+    return "NOTEQ";
+  case TokenType::LT:
+    return "LT";
+  case TokenType::LTEQ:
+    return "LTEQ";
+  case TokenType::GT:
+    return "GT";
+  case TokenType::GTEQ:
+    return "GTEQ";
+  default:
+    return "";
   }
-  else
-  {
-    std::cout << "Token is not EQ" << std::endl;
-  }
-
-  // Print the underlying integer value
-  std::cout << "Token value: " << static_cast<int>(token) << std::endl;
 }
-int main()
-{
+class Token {
+private:
+  std::string tokenText;
+  TokenType tokenKind;
 
-  TokenType token = TokenType::EQ;
-  printTokenType(token);
+public:
+  Token(std::string text, TokenType kind) : tokenText(text), tokenKind(kind) {}
+  Token() : tokenText("\0"), tokenKind(TokenType::EOF_) {}
+  std::string getTokenText() const { return tokenText; }
+  TokenType getTokenKind() const { return tokenKind; }
+  std::optional<TokenType> checkIfKeyword(const std::string &tokenText) {
+    static const TokenType allTokenTypes[] = {
+        TokenType::EOF_,     TokenType::NEWLINE,  TokenType::NUMBER,
+        TokenType::IDENT,    TokenType::STRING,   TokenType::LABEL,
+        TokenType::GOTO,     TokenType::PRINT,    TokenType::INPUT,
+        TokenType::LET,      TokenType::IF,       TokenType::THEN,
+        TokenType::ENDIF,    TokenType::WHILE,    TokenType::REPEAT,
+        TokenType::ENDWHILE, TokenType::EQ,       TokenType::PLUS,
+        TokenType::MINUS,    TokenType::ASTERISK, TokenType::SLASH,
+        TokenType::EQEQ,     TokenType::NOTEQ,    TokenType::LT,
+        TokenType::LTEQ,     TokenType::GT,       TokenType::GTEQ};
 
-  token = TokenType::PLUS;
-  printTokenType(token);
-  std::cout << "done";
+    for (const auto &kind : allTokenTypes) {
+      int value = static_cast<int>(kind);
+      if (tokenTypeToString(kind) == tokenText && value >= 100 && value < 200) {
+        return kind;
+      }
+    }
+    return std::nullopt;
+  }
+};
+
+class LEX {
+
+private:
+  std::string source;
+  char curChar;
+  int curPos;
+
+public:
+  LEX(const std::string &src) : source(src + '\n'), curChar('\0'), curPos(-1) {
+    nextChar();
+  };
+  void nextChar() {
+    curPos++;
+    if (curPos >= static_cast<int>(source.length())) {
+      curChar = '\0';
+    } else {
+      curChar = source[curPos];
+    }
+  }
+  char peek() {
+    if (curPos + 1 >= static_cast<int>(source.length())) {
+      return '\0';
+    }
+    return source[curPos + 1];
+  }
+  void skipWhitespace() {
+    while (curChar == ' ' || curChar == '\t' || curChar == '\r') {
+      nextChar();
+    }
+  };
+  void skipComment() {
+    if (curChar == '#') {
+      while (curChar == '\n') {
+        nextChar();
+      };
+    }
+  }
+  Token getToken() {
+    skipWhitespace();
+    skipComment();
+    Token token;
+    if (curChar == '+') {
+      token = Token(curChar, TokenType::PLUS);
+    } else if (curChar == '-') {
+      token = Token(curChar, TokenType::MINUS);
+    } else if (curChar == '*') {
+      token = Token(curChar, TokenType::ASTERISK);
+    } else if (curChar == '/') {
+      token = Token(curChar, TokenType::SLASH);
+    } else if (curChar == '\n') {
+      token = Token(curChar, TokenType::NEWLINE);
+    } else if (curChar == '\0') {
+      token = Token(curChar, TokenType::EOF_);
+    } else if (curChar == '=') {
+      if (peek() == '=') {
+        char lastchar = curChar;
+        nextChar();
+        token = Token(lastchar + curChar, TokenType::EQEQ);
+      }
+    }
+
+    return token;
+  }
+};
+
+int main() {
+  // The string to be tokenized
+  std::string input = "+==-";
+
+  std::cout << "Tokenizing: \"" << input << "\"" << std::endl;
+
+  LEX lexer(input);
+  Token token;
+
+  do {
+    token = lexer.getToken();
+    std::string tokenTypeStr = tokenTypeToString(token.getTokenKind());
+    std::cout << "Token: " << token.getTokenText() << ", Type: " << tokenTypeStr
+              << std::endl;
+    lexer.nextChar();
+  } while (token.getTokenKind() != TokenType::EOF_);
+
   return 0;
 }
